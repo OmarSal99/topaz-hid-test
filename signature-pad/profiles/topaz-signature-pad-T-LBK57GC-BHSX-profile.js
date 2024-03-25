97e3ef8a3b766796817cece5dded44a571cdcd5a
OmarSaleh <omar.saleh@exalt.corp-partner.google.com>
2024-03-26 00:03:22 +0200

    fix reading

--- signature-pad/profiles/topaz-signature-pad-T-LBK57GC-BHSX-profile.js
+++ signature-pad/profiles/topaz-signature-pad-T-LBK57GC-BHSX-profile.js
@@ -26,9 +26,9 @@ export class TopazSignaturePadTLBK57GCBHSXProfile extends BaseProfile {
 
   static decodeFunction = (bytes) => {
     bytes = bytes.slice(1);
-    if (bytes[0] != 0xc1) {
-      return { x: null, y: null, invalid: true};
-    }
+    if (bytes[0] != 0xc1 && bytes[0] != 0xc0)
+      return { x: null, y: null, invalid: true, ignore: true };
+    if (bytes[0] != 0xc1) return { x: null, y: null, invalid: true };
     let bytesObj = super.decodeFunction(bytes);
     if (bytesObj.x != null) bytesObj.x = bytesObj.x - this.leftCoordinate;
     if (bytesObj.y != null) bytesObj.y = bytesObj.y - this.topCoordinate;
